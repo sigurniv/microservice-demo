269c2a3bed22f06e36f9110b4e0a783a0775e6be
sigurniv <sigurniv@gmail.com>
2020-07-27 12:41:18 +0300

    gateway service structure refactoring

---
+++ service/gateway/cmd/api/internal/server/response.go
@@ -0,0 +1,28 @@
+package server
+
+
+type InfoData struct {
+	Name string `json:"name"`
+}
+
+type InfoResponse struct {
+	Data  InfoData `json:"data"`
+	Error string   `json:"error"`
+}
+
+type Response struct {
+	Data  interface{} `json:"data"`
+	Error string      `json:"error"`
+}
+
+type Tags struct {
+	Tags []string `json:"tags"`
+}
+
+type TagsResponse struct {
+	Data Tags `json:"data"`
+}
+
+type ErrResponse struct {
+	Error string `json:"error"`
+}
